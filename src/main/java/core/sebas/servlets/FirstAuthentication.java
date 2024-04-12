61c1939b92f98085c8ccba04facc83f59cdfabc8
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-04-12 16:41:24 -0400

    Update FirstAuthentication.java

--- src/main/java/core/sebas/servlets/FirstAuthentication.java
+++ src/main/java/core/sebas/servlets/FirstAuthentication.java
@@ -1,103 +1,93 @@
-package core.sebas.servlets;
+import java.sql.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+public class Authentication extends HttpServlet {
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+    private static final Logger log = Logger.getLogger(Authentication.class);
 
-public class FirstAuthentication extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
+        PropertyConfigurator.configure("log4j.properties");
 
-	static {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (Exception e) {}
-	}
+        HttpSession session = request.getSession();
 
-	@Override
-	protected void doPost(HttpServletRequest request,final HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        log.debug("Login user: " + username);
+        
+        boolean success = false;
 
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
+        String query = "select * from user where username=? and password = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        Properties prop = new Properties();
 
-		boolean success = false;
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
+        if (inputStream != null) {
+            prop.load(inputStream);
+        }
+        else {   
+            log.error("property file config.properties not found in the classpath");
+            throw new FileNotFoundException("property file config.properties not found in the classpath");
+        }
+        
+        String userDB = prop.getProperty("userDB");
+        String pwdDB = prop.getProperty("pwdDB");
+        String hostDB = prop.getProperty("hostDB");
+        String portDB = prop.getProperty("portDB");
+        
+        String connectionChain = "jdbc:mysql://" + hostDB + ":" + portDB + "/chess?serverTimezone=UTC&useUnicode=true&characterEncoding=UTF-8";
+        
+        log.debug("Connecting to database with credentials: " + userDB + " and password: ********");
+        log.debug("Database chain: " + connectionChain);
+        try {
+            conn = DriverManager.getConnection(connectionChain, userDB, pwdDB);
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            
+            ResultSet rs = pstmt.executeQuery();  
 
-		Connection conn = null;
-		Statement stmt = null;
-		Properties prop = new Properties();
+            List<String> list = new ArrayList<String>();							
+            while (rs.next()) {
+                // Login Successful if match is found
+                success = true;
 
-		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties");
-		if (inputStream != null) {
-			prop.load(inputStream);
-		}
-		else {   
-			throw new FileNotFoundException("property file config.properties not found in the classpath");
-		}
-		
-		String userDB = prop.getProperty("userDB");
-		String pwdDB = prop.getProperty("pwdDB");
-		String hostDB = prop.getProperty("hostDB");
-		String portDB = prop.getProperty("portDB");
-		
-		String connectionChain = "jdbc:mysql://" + hostDB + ":" + portDB + "/chess?serverTimezone=UTC";
-		try {
-			conn = DriverManager.getConnection(connectionChain, userDB, pwdDB);
-			stmt = conn.createStatement();
-			
-		    PreparedStatement sqlStatement = conn.prepareStatement("select username,password from secure_user "
-		    		+ "where username=? and password=SHA1(?)");
-		    sqlStatement.setString(1, username);
-		    sqlStatement.setString(2, password);
-		    ResultSet rs = sqlStatement.executeQuery();
-			
-			List<String> list = new ArrayList<String>();							
-			while (rs.next()) {
-				// Login Successful if match is found
-				success = true;
+                list.add(rs.getString("username"));
+                list.add(rs.getString("password"));
+                session.setAttribute("user", list);
+            }
+        } catch (Exception e) {
+            log.error("Error to database: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                pstmt.close();
+                conn.close();
+            } catch (Exception e) {
+                log.error("Error closing connection: " + e.getMessage());
+            }
+        }
+        
+        RequestDispatcher requestDispatcher = null;
 
-				list.add(rs.getString("username"));
-				list.add(rs.getString("password"));
-				session.setAttribute("user", list);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				stmt.close();
-				conn.close();
-			} catch (Exception e) {}
-		}
-		
-		RequestDispatcher requestDispatcher = null;
-
-		if (success) {
-			requestDispatcher = request.getRequestDispatcher("/html/login.jsp");
-			requestDispatcher.forward(request, response);
-		} else {
-			session.setAttribute("error", "Username or Password incorrect");
-			requestDispatcher = request.getRequestDispatcher("/html/firstlogin.jsp");
-			requestDispatcher.forward(request, response);
-		}
-	}
+        if (success) {
+            requestDispatcher = request.getRequestDispatcher("/html/timer.jsp");
+            requestDispatcher.forward(request, response);
+        } else {
+            log.error("Error in the connection to database chess");
+            session.setAttribute("error", "Username or Password incorrect");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            requestDispatcher = request.getRequestDispatcher("/html/login.jsp");
+            requestDispatcher.forward(request, response);
+        }
+    }
+}
 	
 }
